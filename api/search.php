7849cc811265f2b0619ab9d3f499b080f486c578
Rahul Sondhi <rahul.sondhi@hotmail.com>
2018-04-24 17:51:38 -0400

    Searching done

--- api/search.php
+++ api/search.php
@@ -4,7 +4,26 @@ function searchProf($search){
   include("start.php");
 
     $info = array();
-    $sql="SELECT * FROM Profile WHERE ProfileID like '$search'";
+    $sql="SELECT * FROM Profile WHERE ProfileID like '%$search%'";
+    $result = mysqli_query($conn,$sql);
+    $count= mysqli_num_rows($result);
+
+    if($count > 0){
+      while ($row = mysqli_fetch_assoc($result)) {
+        $info[] = $row;
+      }
+    }else{
+      $info = "Unvalid";
+    }
+
+    print_r(JSON_encode($info));
+}
+
+function searchAll(){
+  include("start.php");
+
+    $info = array();
+    $sql="SELECT * FROM Profile";
     $result = mysqli_query($conn,$sql);
 
     $count= mysqli_num_rows($result);
